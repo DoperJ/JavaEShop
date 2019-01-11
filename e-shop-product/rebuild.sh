00f555a07d91d8f2fe2f3b9bf8a10840cab484e9
DoperJ <894661167@qq.xom>
2019-01-11 11:16:17 +0800

    购物车🛍

--- e-shop-product/rebuild.sh
+++ e-shop-product/rebuild.sh
@@ -3,4 +3,4 @@ docker stop product
 docker rm product
 docker rmi doperj/e-shop-product
 mvn clean package -Dmaven.test.skip=true docker:build
-docker run -d --name product -p 8081:8081 doperj/e-shop-product
+docker run -d --name product -p 8082:8082 doperj/e-shop-product
